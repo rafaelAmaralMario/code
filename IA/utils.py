1ee1750b7940837621ed2b9162013baf542c8aaa
Rafael Do Amaral Mario <rafaelamaralmario@gmail.com>
2023-06-19 16:27:54 -0300

    fix

--- IA/utils.py
+++ IA/utils.py
@@ -1,3 +1,10 @@
+import PyPDF2
+from docx import Document
+import csv
+import pandas as pd
+import mimetypes
+
+
 def calculate_num_timesteps_audio(input_data, sample_rate, timestep_duration):
     # Cálculo do num_timesteps para dados de áudio
     # input_data: array com os dados de áudio
@@ -52,3 +59,110 @@ def calculate_num_timesteps_file(input_data, timestep_size):
     file_size = len(input_data)  # Tamanho total do arquivo
     num_timesteps = int(file_size / timestep_size)  # Cálculo do num_timesteps
     return num_timesteps
+
+def calculate_num_timesteps_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        pdf = PyPDF2.PdfFileReader(file)
+        num_pages = pdf.getNumPages()
+        return num_pages
+
+
+def calculate_num_timesteps_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        num_rows = sum(1 for row in csv_reader)
+        return num_rows
+
+def calculate_num_timesteps_excel(file_path):
+    excel_data = pd.read_excel(file_path)
+    num_rows = excel_data.shape[0]
+    return num_rows
+
+def calculate_num_timesteps_excel(file_path):
+    excel_data = pd.read_excel(file_path)
+    num_rows = excel_data.shape[0]
+    return num_rows
+
+def calculate_num_timesteps_word(file_path):
+    doc = Document(file_path)
+    num_paragraphs = len(doc.paragraphs)
+    return num_paragraphs
+
+
+def extract_text_from_pdf(file):
+    text = ""
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    num_pages = pdf_reader.getNumPages()
+    for page_number in range(num_pages):
+        page = pdf_reader.getPage(page_number)
+        text += page.extractText()
+    return text
+
+def extract_text_from_docx(file):
+    text = ""
+    doc = Document(file)
+    for paragraph in doc.paragraphs:
+        text += paragraph.text + " "
+    return text
+
+def get_data_type(input_data):
+    # Cria uma instância do objeto Magic
+    
+    content_type, encoding = mimetypes.guess_type(input_data)
+
+    if content_type.startswith('image'):
+        return 'Imagens', 'image'
+    elif content_type.startswith('video'):
+        return 'Vídeos', 'video'
+    elif content_type.startswith('audio'):
+        return 'Áudio', 'audio'
+    elif content_type.startswith('text'):
+        return 'Texto', 'text'
+    else:
+        if content_type == 'application/pdf':
+            return 'Arquivos', 'pdf'
+        elif content_type in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+            return 'Arquivos', 'xls'
+        elif content_type == 'text/csv':
+            return 'Arquivos', 'csv'
+        elif content_type in ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+            return 'Arquivos', 'doc'
+        else:   
+            return 'Arquivos', 'file'
+        
+def get_numtime_steps(input_data, data_subtype):
+    if data_subtype == 'audio':
+        # Calcular o num_timesteps específico para dados de áudio
+        return calculate_num_timesteps_audio(input_data)
+    elif data_subtype == 'video':
+        # Calcular o num_timesteps específico para dados de vídeo
+        return calculate_num_timesteps_video(input_data)
+    elif data_subtype == 'image':
+        # Calcular o num_timesteps específico para dados de imagem
+        return calculate_num_timesteps_image(input_data)
+    elif data_subtype == 'text':
+        # Calcular o num_timesteps específico para dados de texto
+        return calculate_num_timesteps_text(input_data)
+    elif data_subtype == 'pdf':
+        # Calcular o num_timesteps específico para outros tipos de arquivo
+        return calculate_num_timesteps_pdf(input_data)
+    elif data_subtype == 'xls':
+         # Calcular o num_timesteps específico para outros tipos de arquivo
+        return calculate_num_timesteps_excel(input_data)
+    elif data_subtype == 'csv':
+        # Calcular o num_timesteps específico para outros tipos de arquivo
+        return calculate_num_timesteps_csv(input_data)
+    elif data_subtype == 'doc':
+        # Calcular o num_timesteps específico para outros tipos de arquivo
+        return calculate_num_timesteps_word(input_data)
+    elif data_subtype == 'file':
+        # Calcular o num_timesteps específico para outros tipos de arquivo
+        return calculate_num_timesteps_file(input_data)
+
+def get_extracted_data(input_data, subtype):
+    if subtype == 'pdf':
+        return extract_text_from_pdf(input_data)
+    elif subtype == 'doc':
+        return extract_text_from_docx(input_data)
+    else:
+        return input_data
